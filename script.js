b9b08ea92b6fac37fcc96e567259ba3e46a6e639
Nicci <hello@nicciyin.com>
2025-08-09 17:50:31 -0700

    index changes

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ $(document).ready(function() {
 
     $('.prompt').hover(
         function() {
-            // On mouse enter - start animation
-            $(this).prev('.star').css('animation', 'rotateAnimation 3s linear infinite');
-            $(this).prev('.star').css('color', '#0000ff');
+            const $star = $(this).prev('.star');
+            $star.css('animation', 'rotateAnimation 3s linear infinite');
+            if ($(this).find('a').length > 0) {
+                $star.css('color', '#0000ff');
+            }
         },
         function() {
-            // On mouse leave - stop animation
-            $(this).prev('.star').css('animation', 'none');
-            $(this).prev('.star').css('color', '#000000');
+            const $star = $(this).prev('.star');
+            $star.css('animation', 'none');
+            $star.css('color', '#000000');
         }
     );
 });
